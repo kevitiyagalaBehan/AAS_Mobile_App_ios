9cc9437a741dba17ddea6334813600ef61a2e8a9
kevitiyagalaBehan <kevitiyagalabehan@gmail.com>
2025-09-24 12:30:22 +0530

    updated styles

--- components/Drawer.tsx
+++ components/Drawer.tsx
@@ -1,25 +1,24 @@
-import { StyleSheet, TouchableOpacity, useWindowDimensions } from "react-native";
+import { StyleSheet, TouchableOpacity, useWindowDimensions, View } from "react-native";
 import React from "react";
 import { DrawerActions } from "@react-navigation/native";
 import { useNavigation } from "@react-navigation/native";
 import Ionicons from "react-native-vector-icons/Ionicons";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Drawer() {
   const navigation = useNavigation();
   const { width, height } = useWindowDimensions();
 
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={styles.container}>
       <TouchableOpacity
         onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
-        style={{
-          paddingTop: height * 0.02,
-        }}
+        // style={{
+        //   paddingTop: height * 0.02,
+        // }}
       >
         <Ionicons name="menu" size={30} color="#1B77BE" />
       </TouchableOpacity>
-    </SafeAreaView>
+    </View>
   );
 }
 
