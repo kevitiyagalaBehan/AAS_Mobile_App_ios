0551665673a874704d91fe276e55633e16bdd6f0
kevitiyagalaBehan <kevitiyagalabehan@gmail.com>
2025-11-26 15:02:20 +0530

    Root navigation

---
+++ src/navigation/RootNavigation.ts
@@ -0,0 +1,13 @@
+import { createNavigationContainerRef } from '@react-navigation/native';
+import { RootStackParamList } from './types';
+
+export const navigationRef = createNavigationContainerRef<RootStackParamList>();
+
+export function navigate(
+  name: keyof RootStackParamList,
+  params?: object
+) {
+  if (navigationRef.isReady()) {
+    navigationRef.navigate(name as any, params as any);
+  }
+}
