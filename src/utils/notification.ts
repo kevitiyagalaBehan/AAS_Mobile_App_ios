feb39bb25dca76d4db1f20f3736b0c51362e3b69
kevitiyagalaBehan <kevitiyagalabehan@gmail.com>
2025-11-26 15:02:40 +0530

    Push Notifications

---
+++ src/utils/notification.ts
@@ -0,0 +1,49 @@
+import Constants from "expo-constants";
+import * as Notifications from "expo-notifications";
+import * as Device from 'expo-device';
+import { Platform } from "react-native";
+
+export type ExpoPushToken = string | null;
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+    shouldShowBanner: true,
+    shouldShowList: true,
+  }),
+});
+
+function handleRegistrationError(errorMessage: string) {
+  alert(errorMessage);
+  throw new Error(errorMessage);
+}
+
+export async function registerForPushNotificationsAsync(): Promise<ExpoPushToken> {
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+
+    if (finalStatus !== "granted") {
+      handleRegistrationError("Push notifications permission not granted.");
+      return null;
+    }
+
+    const token = (
+      await Notifications.getExpoPushTokenAsync({
+        projectId: Constants.expoConfig?.extra?.eas?.projectId,
+      })
+    ).data;
+
+    return token;
+
+  } else {
+    handleRegistrationError('Must use physical device for push notifications');
+    return null;
+  }
+}
